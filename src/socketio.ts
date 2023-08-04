33fa4a9da521ef8cc4b138d8a4278a7a31bbcf50
XPA <xpadev@gmail.com>
2023-08-04 17:50:18 +0900

    fix: support cors

--- src/socketio.ts
+++ src/socketio.ts
@@ -3,9 +3,15 @@ import * as https from "https";
 import { onConnectHandler } from "@/socket";
 import { ClientToServerEvents } from "@/@types/socket";
 import { DefaultEventsMap } from "socket.io/dist/typed-events";
+import { ExpressCorsHost } from "@/config";
 
 const setupSocketIO = (server: https.Server<any, any>) => {
-  const io = new Server<ClientToServerEvents, DefaultEventsMap>(server);
+  const io = new Server<ClientToServerEvents, DefaultEventsMap>(server, {
+    cors: {
+      origin: ExpressCorsHost,
+      methods: ["GET", "POST"],
+    },
+  });
   io.on("connection", onConnectHandler);
 };
 
